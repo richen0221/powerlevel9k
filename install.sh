1a804d7c68e52303123d5a283586c8c0b3787499
Ricky Chen <ricky0221.work@gmail.com>
2018-06-10 15:14:59 +0800

    add the font file back

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && apt install zsh curl konsole -y
+sudo apt update && sudo apt install zsh curl konsole -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 cp -f zshrc $HOME/.zshrc
@@ -7,4 +7,4 @@ mkdir -p ~/.local/share/konsole
 cp -f zsh_powerlevel9k.profile ~/.local/share/konsole/zsh_powerlevel9k.profile
 mkdir ~/.fonts
 mv fonts/*.ttf ~/.fonts/
-fc-cache -vf
+fc-cache -v
