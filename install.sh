6f3d96733df6ad1d143f374c82da3bd60f8721e0
Ricky Chen <ricky0221.work@gmail.com>
2018-06-10 14:43:35 +0800

    add fonts

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo apt update && apt install zsh curl -y
+sudo apt update && apt install zsh curl konsole -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+cp -f zshrc $HOME/.zshrc
+mkdir -p ~/.local/share/konsole
+cp -f zsh_powerlevel9k.profile ~/.local/share/konsole/zsh_powerlevel9k.profile
+mkdir ~/.fonts
+mv fonts/*.ttf ~/.fonts/
+fc-cache -vf
---
+++ zsh_powerlevel9k.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Linux
+Font=DejaVuSansMono Nerd Font,14,-1,5,50,0,0,0,0,0
+
+[General]
+Command=/bin/zsh
+Name=zsh_powerlevel9k
+Parent=FALLBACK/
